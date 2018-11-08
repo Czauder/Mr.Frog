3f88099dca5bcd2f383c2a7449edf39aba2067e1
Przemek Czauderna <przemek.czauderna@gmail.com>
2018-11-08 13:59:23 +0100

    add html style css and js

--- main.js
+++ main.js
@@ -1,50 +1,69 @@
 // główna część
-const problemAdd = []
-const possibilities = ["Just love..", "You must take credit for it!", "You must go to to the Gym!", "just hard work!"]
-const btnAdd = document.querySelector('.addProblem')
-const btnAdv = document.querySelector('.advice')
+const problemsAdd = []
+const possibilities = ["Easyyyy! just love..", "You must take credit for it!", "You must go to to the Gym!", "Just hard work!"];
+const btnAdd = document.querySelector('.addProblem');
+const btnAdv = document.querySelector('.advice');
+const inputProblem = document.querySelector('.problem');
+const possibilitiesAdd = document.querySelector('.possibilities');
+const div = document.querySelector('div');
+const divsAnimation = document.querySelectorAll('div')
+
 
 
 btnAdd.addEventListener('click', () => {
-  const inputProblem = document.querySelector('.problem')
   const nameProblem = inputProblem.value;
-  problemAdd.push(nameProblem)
-  console.log(inputProblem.value);
+  if (nameProblem) {
+    problemsAdd.push(nameProblem)
+  } else {
+    alert("really?!\n\n You don't have problem!?\n\n Write Your problem...");
+    return
+  }
   inputProblem.value = ""
+  console.log(nameProblem);
 
 })
 
-btnAdv.addEventListener('click', () => {
-  const possibilitiesAdd = document.querySelector('.possibilities')
+const adviceResult = () => {
   const namePas = possibilitiesAdd.value;
-  possibilities.push(namePas)
-  console.log(possibilitiesAdd.value);
-  possibilitiesAdd.value = ""
-})
+  if (problemsAdd > "" && namePas) {
+    possibilities.push(namePas);
+    showResult()
+  } else if (problemsAdd > "" && namePas === "") {
+    alert("really?!?\n\n You don't have idea!?\n\n Write Your idea.. ")
+  } else alert(" 1.Write Your problem..\n\n 2.Write Your idea..  ")
+  possibilitiesAdd.value = "";
+}
 
+btnAdv.addEventListener('click', adviceResult)
 
-
-btnAdv.addEventListener('click', () => {
+const showResult = () => {
   const indexName = (Math.floor(Math.random() * possibilities.length));
-  const indexNameA = (Math.floor(Math.random() * problemAdd.length));
-
+  const indexNameA = (Math.floor(Math.random() * problemsAdd.length));
+  div.textContent = `hmm.. ${problemsAdd[indexNameA]} so..  ${possibilities[indexName]}`
   console.log(indexName);
-  const div = document.querySelector('div')
-
-  div.textContent = `hmm.. ${problemAdd[indexNameA]} so..  ${possibilities[indexName]}`
-})
-
-// próba podłączenie enter
-
-// add key enter for 'advice!'
-const input = document.querySelector('.possibilities')
-
-const giveSolution = (e) => {
-  if (e.keyCode === 13) {
-    console.log("działa")
-  } else return
-
 }
 
 
-input.addEventListener('keydown', giveSolution)
+
+// add key enter for button 'advice!'
+const enterKey = (e) => {
+  if (e.keyCode === 13) {
+    showResult(e);
+    adviceResult(e);
+    console.log("działa")
+  } else return
+}
+
+
+possibilitiesAdd.addEventListener('keydown', enterKey)
+// do analizy^^^ nie działa tak jak trzeba 
+
+
+
+const welcomeTextAnimation = () => {
+  divsAnimation.forEach((div) => {
+    div.classList.add("active");
+  })
+
+}
+setTimeout(welcomeTextAnimation, 2000)
